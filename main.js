330767fb93d9688cb5168ae81a2e0e299612488a
JRKreutzer <jose.kreutzer@gmail.com>
2023-11-23 17:12:23 -0300

    Acessiblidade, space enter, e tratamento de erros

--- main.js
+++ main.js
@@ -2,8 +2,16 @@
 //     document.querySelector('#som_tecla_pom').play();
 // }
 
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+        //elemento !== null ====> elemento  Mesma coisa
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 // document.querySelector('.tecla_pom').onclick = tocaSomPom;
@@ -22,6 +30,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function(){
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+    
+        if (evento.keyCode === 13 || evento.keyCode === 32) {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
     }
 
 }
